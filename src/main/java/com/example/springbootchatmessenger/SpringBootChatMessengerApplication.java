83e0d53952099b32f5ee3041bef4aa6c4f143893
morteza363831 <morteza363831official@gmail.com>
2024-10-10 18:41:22 +0330

    what I did in the commit : Initialized project .
    what I did change :
    I added some dependencies to pom.xml
    
    -->spring-web
    -->spring-jpa-data
    -->spring-websocket
    -->spring-thymeleaf
    -->spring-kafka
    -->spring-security
    -->lombok
    -->spring-test
    -->h2-db
    -->modelmapper
    -->jjwt
    -->spring-oauth2

---
+++ src/main/java/com/example/springbootchatmessenger/SpringBootChatMessengerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootchatmessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChatMessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootChatMessengerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootchatmessenger/SpringBootChatMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootchatmessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootChatMessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
