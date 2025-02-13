e6ff38948141ae2f54de00600ce6d6ac6b3979b9
morteza363831 <morteza363831official@gmail.com>
2025-02-13 13:30:16 +0330

    application.properties & Config & pom.xml updated .
    Keycloak settings commented on application.properties
    
    HttpClient Bean added in Config class .
    ObjectMapper Bean added in Config class .
    
    pom.xml now use war packaging to be use in Tomcat

--- src/main/java/com/example/springbootchatmessenger/httpconfig/Config.java
+++ src/main/java/com/example/springbootchatmessenger/httpconfig/Config.java
@@ -1,10 +1,14 @@
 package com.example.springbootchatmessenger.httpconfig;
 
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.http.HttpClient;
 
 
 /*
@@ -20,6 +24,16 @@ public class Config {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public HttpClient httpClient() {
+        return HttpClient.newHttpClient();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL);
     }
 
 }
