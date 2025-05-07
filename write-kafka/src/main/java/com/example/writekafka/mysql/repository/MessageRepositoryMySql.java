fe953aac1f1c5e54a7e0f30b5f3b9963b84247ec
morteza363831 <morteza363831official@gmail.com>
2025-05-07 17:19:05 +0330

    repositories for my sql added

---
+++ write-kafka/src/main/java/com/example/writekafka/mysql/repository/MessageRepositoryMySql.java
@@ -0,0 +1,19 @@
+package com.example.writekafka.mysql.repository;
+
+import com.example.writekafka.model.Message;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface MessageRepositoryMySql extends JpaRepository<Message, UUID> {
+
+    @Transactional(rollbackOn = Exception.class)
+    @Query("SELECT m FROM Message m WHERE m.sessionId = :sessionId")
+    Optional<Message> findBySessionId(@Param("sessionId") final UUID sessionId);
+}
