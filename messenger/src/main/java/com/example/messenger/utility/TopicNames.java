5ecc5f62fe2a90cabe7a290dc9300c654de81226
morteza363831 <morteza363831official@gmail.com>
2025-05-07 17:20:01 +0330

    TopicNames added (to configure topics in structural

---
+++ messenger/src/main/java/com/example/messenger/utility/TopicNames.java
@@ -0,0 +1,16 @@
+package com.example.messenger.utility;
+
+public class TopicNames {
+
+    public static String USER_WRITE_TOPIC = "user-write-topic";
+
+    public static String USER_READ_TOPIC = "user-read-topic";
+
+    public static String SESSION_WRITE_TOPIC = "session-write-topic";
+
+    public static String SESSION_READ_TOPIC = "session-read-topic";
+
+    public static String MESSAGE_WRITE_TOPIC = "message-write-topic";
+
+    public static String MESSAGE_READ_TOPIC = "message-read-topic";
+}
