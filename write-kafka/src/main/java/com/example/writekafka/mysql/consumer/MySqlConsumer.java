75ee477b178634917cefc5aec70b98bdadd69046
morteza363831 <morteza363831official@gmail.com>
2025-05-08 10:09:34 +0330

    MySqlConsumerConfig and MySqlConsumer will listen to events . -> just mysql events

---
+++ write-kafka/src/main/java/com/example/writekafka/mysql/consumer/MySqlConsumer.java
@@ -0,0 +1,32 @@
+package com.example.writekafka.mysql.consumer;
+
+import com.example.messenger.kafka.KafkaDataStructure;
+import com.example.messenger.utility.TopicNames;
+import com.example.writekafka.mysql.handler.MySqlCommandDispatcher;
+import com.example.writekafka.utility.ConsumerTypes;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@EnableKafka
+@Component
+@RequiredArgsConstructor
+public class MySqlConsumer {
+
+
+    private final MySqlCommandDispatcher dispatcher;
+
+    @KafkaListener(topics = {
+            TopicNames.USER_WRITE_TOPIC,
+            TopicNames.SESSION_WRITE_TOPIC,
+            TopicNames.MESSAGE_WRITE_TOPIC },
+            groupId = ConsumerTypes.MYSQL_GROUP)
+    public void listen(KafkaDataStructure kafkaDataStructure) {
+        log.info("Received: {}", kafkaDataStructure);
+        dispatcher.dispatch(kafkaDataStructure);
+    }
+
+}
