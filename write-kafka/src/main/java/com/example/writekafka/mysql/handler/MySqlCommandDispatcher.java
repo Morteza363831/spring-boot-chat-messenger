78c4a6b035a1e974845a50ae1d0caefad23e4e46
morteza363831 <morteza363831official@gmail.com>
2025-05-08 10:08:57 +0330

    MySqlCommandDispatcher will direct command requests to right handler using factory

---
+++ write-kafka/src/main/java/com/example/writekafka/mysql/handler/MySqlCommandDispatcher.java
@@ -0,0 +1,62 @@
+package com.example.writekafka.mysql.handler;
+
+import com.example.messenger.kafka.KafkaDataStructure;
+import com.example.messenger.utility.DataTypes;
+import com.example.messenger.utility.RequestTypes;
+import com.example.writekafka.model.Message;
+import com.example.writekafka.model.Session;
+import com.example.writekafka.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ *  Dispatchers are same for now ! (because we are using two relational database)
+ * */
+
+@Service
+@RequiredArgsConstructor
+public class MySqlCommandDispatcher {
+
+
+    private final MySqlCommandHandlerFactory mySqlCommandHandlerFactory;
+
+    public void dispatch(KafkaDataStructure kafkaDataStructure) {
+        DataTypes dataType = kafkaDataStructure.getDataType();
+        RequestTypes requestType = kafkaDataStructure.getRequestType();
+
+        switch (dataType) {
+            case USER -> mySqlCommandHandlerFactory.<User>getHandler(DataTypes.USER)
+                    .handle(requestType, convertToUser(kafkaDataStructure.getData()));
+
+            case SESSION -> mySqlCommandHandlerFactory.<Session>getHandler(DataTypes.SESSION)
+                    .handle(requestType, convertToSession(kafkaDataStructure.getData()));
+
+            case MESSAGE -> mySqlCommandHandlerFactory.<Message>getHandler(DataTypes.MESSAGE)
+                    .handle(requestType, convertToMessage(kafkaDataStructure.getData()));
+
+            default -> throw new RuntimeException("Unexpected value: " + dataType);
+        }
+    }
+
+
+    private User convertToUser(Object data) {
+        if (data instanceof User) {
+            return (User) data;
+        }
+        return null;
+    }
+
+    private Session convertToSession(Object data) {
+        if (data instanceof Session) {
+            return (Session) data;
+        }
+        return null;
+    }
+
+    private Message convertToMessage(Object data) {
+        if (data instanceof Message) {
+            return (Message) data;
+        }
+        return null;
+    }
+}
