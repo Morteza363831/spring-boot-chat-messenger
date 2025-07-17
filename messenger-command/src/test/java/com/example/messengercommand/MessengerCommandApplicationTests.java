d7799808730fd5e493c8bf72c8e9951f1669d122
morteza363831 <morteza363831official@gmail.com>
2025-07-17 19:10:22 +0330

    WriteKafkaApplication renamed to MessengerCommandApplication include its test classes

--- messenger-command/src/test/java/com/example/messengercommand/MessengerCommandApplicationTests.java
+++ messenger-command/src/test/java/com/example/messengercommand/MessengerCommandApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WriteKafkaApplicationTests {
+class MessengerCommandApplicationTests {
 
     @Test
     void contextLoads() {
