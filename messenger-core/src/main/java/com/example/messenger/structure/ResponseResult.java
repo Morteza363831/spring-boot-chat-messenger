072bbe2320f7f45d6dbc2cf3e6da43e7e84dcaa3
morteza363831 <morteza363831official@gmail.com>
2025-08-01 23:17:12 +0330

    Some changed applied for user processing logic in modules core and query . It works fine for now . But there is multiple issues we need to solve later .

--- messenger-core/src/main/java/com/example/messenger/structure/ResponseResult.java
+++ messenger-core/src/main/java/com/example/messenger/structure/ResponseResult.java
@@ -1,16 +1,23 @@
 package com.example.messenger.structure;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Value;
 
 import java.util.Date;
 
 @Value
 public class ResponseResult<T> {
+    @JsonProperty("status")
     String status;      // success, error, fail
+    @JsonProperty("status_code")
     int statusCode;     // HTTP status code
+    @JsonProperty("message")
     String message;     // Custom message
+    @JsonProperty("timestamp")
     Date timestamp;     // Response time
+    @JsonProperty("data")
     T data;             // Generic response data
+    @JsonProperty("path")
     String path;        // Endpoint path
 
     public ResponseResult(String status, int statusCode, String message, T data, String path) {
