b87d8fc8212a908f30b076b1da508c9bf46c8ccb
morteza363831 <morteza363831official@gmail.com>
2025-08-01 23:07:33 +0330

    EncryptionUtil added to command module to encrypt important information like authorities and pass

---
+++ messenger-command/src/main/java/com/example/messengercommand/utility/EncryptionUtil.java
@@ -0,0 +1,45 @@
+package com.example.messengercommand.utility;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class EncryptionUtil {
+
+    private final static String ALGORITHM = "AES";
+    private final static byte[] SECRET = {-66, 38, -104, 107, -44, 110, 120, -123, -96, -96, -57, -117, 84, 109, 127, -113};
+
+    public static String encrypt(String plainText) {
+        final SecretKeySpec keySpec = new SecretKeySpec(SECRET, ALGORITHM);
+        try {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(plainText.getBytes()));
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException |
+                 BadPaddingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String decrypt(String cipherText) {
+        final SecretKeySpec key = new SecretKeySpec(SECRET, ALGORITHM);
+        try {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            return new String(cipher.doFinal(Base64.getDecoder().decode(cipherText)));
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException |
+                 BadPaddingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static boolean isEncrypted(String data) {
+        return !data.matches("[A-Za-z0-9+/=]+"); // Checks if Base64 encoded
+    }
+
+}
