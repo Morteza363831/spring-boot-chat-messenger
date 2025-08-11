9ef913de2ffa31068cad15d02f8b8d78ba4ec410
morteza363831 <morteza363831official@gmail.com>
2025-08-11 23:10:40 +0330

    Validations removed from models (it will validate in core module) there is no reason to validate data here for now .

--- messenger-command/src/main/java/com/example/messengercommand/model/Message.java
+++ messenger-command/src/main/java/com/example/messengercommand/model/Message.java
@@ -1,13 +1,10 @@
 package com.example.messengercommand.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import java.util.UUID;
 
 @Getter
 @Setter
@@ -18,21 +15,16 @@ import java.util.UUID;
 })
 public class Message {
     @Id
-    @Size(max = 36)
     @Column(name = "id", nullable = false, length = 36)
     private String id;
 
-    @NotNull
     @Lob
     @Column(name = "content", nullable = false)
     private String content;
 
-    @NotNull
     @Column(name = "session_id", nullable = false)
     private String sessionId;
 
-    @Size(max = 255)
-    @NotNull
     @Column(name = "encrypted_aes_key", nullable = false)
     private String encryptedAesKey;
 
--- messenger-command/src/main/java/com/example/messengercommand/model/Session.java
+++ messenger-command/src/main/java/com/example/messengercommand/model/Session.java
@@ -1,13 +1,9 @@
 package com.example.messengercommand.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
-import java.util.UUID;
 
 @Getter
 @Setter
@@ -15,16 +11,13 @@ import java.util.UUID;
 @Table(name = "sessions")
 public class Session {
     @Id
-    @Size(max = 36)
     @Column(name = "id", nullable = false, length = 36)
     private String id;
 
-    @NotNull
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "user1_id", referencedColumnName = "id", nullable = false)
     private User user1;
 
-    @NotNull
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "user2_id", referencedColumnName = "id", nullable = false)
     private User user2;
