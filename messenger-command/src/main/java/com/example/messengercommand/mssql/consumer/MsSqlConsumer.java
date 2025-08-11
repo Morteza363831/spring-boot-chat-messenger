7d8496612ae699fd785a3ad5bc33c932ed98602f
morteza363831 <morteza363831official@gmail.com>
2025-08-11 23:06:35 +0330

    catch when encounter with exception in consumers . Later I will add some strategies on failure mode (when event fails to finish)

--- messenger-command/src/main/java/com/example/messengercommand/mssql/consumer/MsSqlConsumer.java
+++ messenger-command/src/main/java/com/example/messengercommand/mssql/consumer/MsSqlConsumer.java
@@ -1,5 +1,7 @@
 package com.example.messengercommand.mssql.consumer;
 
+import com.example.messengercommand.aop.AfterThrowingException;
+import com.example.messengercommand.exceptions.KafkaException;
 import com.example.messengerutilities.model.KafkaDataStructure;
 import com.example.messengerutilities.utility.TopicNames;
 import com.example.messengercommand.mssql.handler.MsSqlCommandDispatcher;
@@ -9,24 +11,33 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @EnableKafka
 @Component
 @RequiredArgsConstructor
+@AfterThrowingException
 public class MsSqlConsumer {
 
 
     private final MsSqlCommandDispatcher dispatcher;
 
+    @Transactional(rollbackFor = KafkaException.class)
     @KafkaListener(topics = {
             TopicNames.USER_WRITE_TOPIC,
             TopicNames.SESSION_WRITE_TOPIC,
             TopicNames.MESSAGE_WRITE_TOPIC },
             groupId = ConsumerTypes.MSSQL_GROUP)
     public void listen(KafkaDataStructure kafkaDataStructure) {
-        log.info("Received: {}", kafkaDataStructure);
-        dispatcher.dispatch(kafkaDataStructure);
+        try {
+            log.info("Received: {}", kafkaDataStructure);
+            dispatcher.dispatch(kafkaDataStructure);
+        }
+        catch (Exception e) {
+            //throw new KafkaException("Mssql consumer could not to finish event processing.");
+            log.error("skipped ms");
+        }
     }
 
 }
