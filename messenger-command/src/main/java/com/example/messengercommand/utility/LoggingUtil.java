dcb6feb3b7214fa8edb5f4a44c9a32eedc854096
morteza363831 <morteza363831official@gmail.com>
2025-08-11 22:53:17 +0330

    LoggingUtil strategy added

---
+++ messenger-command/src/main/java/com/example/messengercommand/utility/LoggingUtil.java
@@ -0,0 +1,20 @@
+package com.example.messengercommand.utility;
+
+import org.slf4j.Logger;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoggingUtil {
+
+    public void error(Logger log, String type, String message, String methodName) {
+        log.error("type : {}, message : {}, method : {}", type, message, methodName);
+    }
+
+    public void debug(Logger log, String type, String message, String methodName, Throwable ex) {
+        log.debug("type : {}, message : {}, method : {}, details : ", type, message, methodName, ex);
+    }
+
+    public void info(Logger log, String type, String methodName, Object data) {
+        log.info("type : {}, method : {}, data : {}", type, methodName, data);
+    }
+}
