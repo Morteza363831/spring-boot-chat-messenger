72a9f391a40de4d27625e2bac930de19806ad287
morteza363831 <morteza363831official@gmail.com>
2025-08-12 22:08:10 +0330

    throw UnknownException in EncryptionUtil with dedicated Throwable .

--- messenger-query/src/main/java/com/example/messengerquery/util/EncryptionUtil.java
+++ messenger-query/src/main/java/com/example/messengerquery/util/EncryptionUtil.java
@@ -1,5 +1,7 @@
 package com.example.messengerquery.util;
 
+import com.example.messengerquery.exception.UnknownException;
+
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
@@ -14,18 +16,6 @@ public class EncryptionUtil {
     private final static String ALGORITHM = "AES";
     private final static byte[] SECRET = {-66, 38, -104, 107, -44, 110, 120, -123, -96, -96, -57, -117, 84, 109, 127, -113};
 
-    public static String encrypt(String plainText) {
-        final SecretKeySpec keySpec = new SecretKeySpec(SECRET, ALGORITHM);
-        try {
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, keySpec);
-            return Base64.getEncoder().encodeToString(cipher.doFinal(plainText.getBytes()));
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException |
-                 BadPaddingException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     public static String decrypt(String cipherText) {
         final SecretKeySpec key = new SecretKeySpec(SECRET, ALGORITHM);
         try {
@@ -34,7 +24,7 @@ public class EncryptionUtil {
             return new String(cipher.doFinal(Base64.getDecoder().decode(cipherText)));
         } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException |
                  BadPaddingException e) {
-            throw new RuntimeException(e);
+            throw new UnknownException(e);
         }
     }
 
