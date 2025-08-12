94abf42e12c00e4e97288c19cace9edba2d3d8c0
morteza363831 <morteza363831official@gmail.com>
2025-08-12 22:09:23 +0330

    Validation removed in Models .

--- messenger-query/src/main/java/com/example/messengerquery/model/User.java
+++ messenger-query/src/main/java/com/example/messengerquery/model/User.java
@@ -18,40 +18,30 @@ import org.springframework.data.elasticsearch.annotations.Document;
         @UniqueConstraint(name = "email", columnNames = {"email"})
 })
 public class User {
+
     @Id
-    @Size(max = 36)
     @Column(name = "id", nullable = false, length = 36)
     private String id;
 
-    @Size(max = 25)
-    @NotNull
     @Column(name = "username", nullable = false, length = 25)
     private String username;
 
-    @Size(max = 100)
-    @NotNull
     @Column(name = "email", nullable = false, length = 100)
     private String email;
 
-    @NotNull
     @Column(name = "enabled", nullable = false)
     private Boolean enabled = false;
 
-    @Size(max = 50)
     @Column(name = "first_name", length = 50)
     private String firstName;
 
-    @Size(max = 50)
     @Column(name = "last_name", length = 50)
     private String lastName;
 
-    @NotNull
     @Lob
     @Column(name = "authorities", nullable = false)
     private String authorities;
 
-    @Size(max = 255)
-    @NotNull
     @Column(name = "password", nullable = false)
     private String password;
 
